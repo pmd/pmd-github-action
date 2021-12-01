2aa5090467fc381c2fb482cd9be17e34d7039b4e
Andreas Dangel <andreas.dangel@pmd-code.org>
2021-12-01 17:13:31 +0100

    Create annotations from sarif report

---
+++ tests/annotations.test.js
@@ -0,0 +1,51 @@
+const core = require('@actions/core');
+const path = require('path');
+const sarif = require('../lib/sarif');
+const annotations = require('../lib/annotations');
+
+core.error = jest.fn();
+core.warning = jest.fn();
+core.notice = jest.fn();
+
+describe('pmd-github-action-annotations', function () {
+
+    beforeEach(() => {
+        core.error.mockClear();
+        core.warning.mockClear();
+        core.notice.mockClear();
+    });
+
+    it('can create annotation', () => {
+        const report = sarif.loadReport(path.join(__dirname, 'data', 'pmd-report.sarif'));
+
+        annotations.processSarifReport(report);
+
+        expect(core.notice).toHaveBeenCalledTimes(1);
+        expect(core.notice).toHaveBeenCalledWith('Variable \'x\' defined but not used', {
+            file: '/home/andreas/PMD/source/pmd-github-action-test/src/classes/UnusedLocalVariableSample.cls',
+            startLine: 3
+        });
+        expect(core.error).not.toHaveBeenCalled();
+        expect(core.warning).not.toHaveBeenCalled();
+    });
+
+    it('can deal with null report', () => {
+        annotations.processSarifReport(null);
+        expect(core.notice).not.toHaveBeenCalled();
+    });
+
+    it('can deal with error, warning and notice', () => {
+        const report = sarif.loadReport(path.join(__dirname, 'data', 'pmd-report-priorities.sarif'));
+        annotations.processSarifReport(report);
+
+        expect(core.error).toHaveBeenCalledTimes(2);
+        expect(core.error).toHaveBeenNthCalledWith(1, 'High Prio Rule', { file: '/folder/file1.txt', startLine: 4 });
+        expect(core.error).toHaveBeenNthCalledWith(2, 'Medium High Prio Rule', { file: '/folder/file2.txt', startLine: 5 });
+        expect(core.warning).toHaveBeenCalledTimes(2);
+        expect(core.warning).toHaveBeenNthCalledWith(1, 'Medium Prio Rule', { file: '/folder/file3.txt', startLine: 6 });
+        expect(core.warning).toHaveBeenNthCalledWith(2, 'Medium Low Prio Rule', { file: '/folder/file4.txt', startLine: 7 });
+        expect(core.notice).toHaveBeenCalledTimes(2);
+        expect(core.notice).toHaveBeenNthCalledWith(1, 'Low Prio Rule', { file: '/folder/file5.txt', startLine: 8 });
+        expect(core.notice).toHaveBeenNthCalledWith(2, 'Low Prio Rule', { file: '/folder/file6.txt', startLine: 9 });
+    });
+});
--- tests/sarif.test.js
+++ tests/sarif.test.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const sarif = require('../lib/sarif');
 
-describe('pmd-github-action-sarif', function() {
+describe('pmd-github-action-sarif', function () {
 
   it('can count violations', () => {
     const count = sarif.countViolations(path.join(__dirname, 'data', 'pmd-report.sarif'));
@@ -18,4 +18,14 @@ describe('pmd-github-action-sarif', function() {
     expect(count).toBe(0);
   })
 
+  it('can load report', () => {
+    const report = sarif.loadReport(path.join(__dirname, 'data', 'pmd-report.sarif'));
+    expect(report).not.toBe(null);
+    expect(report.runs[0].tool.driver.name).toBe('PMD');
+  })
+
+  it('can deal with missing report', () => {
+    const report = sarif.loadReport(path.join(__dirname, 'data', 'pmd-report-not-existing.sarif'));
+    expect(report).toBe(null);
+  })
 });
