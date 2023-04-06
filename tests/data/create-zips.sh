6430a27e841349d9c0a8b057edf4fc05dfdaec0b
Andreas Dangel <andreas.dangel@pmd-code.org>
2023-04-06 11:36:21 +0200

    Support new asset names with PMD 7.0.0-rc3

--- tests/data/create-zips.sh
+++ tests/data/create-zips.sh
@@ -4,4 +4,5 @@ zip -r pmd-bin-6.39.0.zip pmd-bin-6.39.0
 zip -r pmd-bin-6.40.0.zip pmd-bin-6.40.0
 zip -r pmd-bin-6.41.0.zip pmd-bin-6.41.0
 zip -r pmd-bin-7.0.0-rc1.zip pmd-bin-7.0.0-rc1
+zip -r pmd-dist-7.0.0-rc3-bin.zip pmd-bin-7.0.0-rc3
 zip -r pmd-bin-7.0.0-SNAPSHOT.zip pmd-bin-7.0.0-SNAPSHOT
